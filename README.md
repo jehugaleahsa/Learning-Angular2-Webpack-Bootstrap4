ddd1826e79f4a13f51cc9be9ec1dccb25162a4b8
jehugaleahsa <jehugaleahsa@gmail.com>
2016-12-21 20:09:44 -0500

    Add optimization options.

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # Learning Angular 2, Webpack and Bootstrap 4
-
 A test project to help me learn Angular 2, Webpack 2 and Bootstrap 4.
 
 ## Setup Instructions
--- webpack.config.js
+++ webpack.config.js
@@ -28,7 +28,7 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, './dist'),
-        filename: '[name].bundle.js',
+        filename: '[name].bundle.[hash].js',
         sourcemapFilename: '[name].map'
     },
     resolve: {
